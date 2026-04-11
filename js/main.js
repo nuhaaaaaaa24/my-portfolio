a9cfaade8712eb6c1afefb953b32023c4b32528c
nuharilwan <cb011094@students.apiit.lk>
2026-04-11 19:27:04 +0530

    Improved CSS and Structure

--- js/main.js
+++ js/main.js
@@ -6,10 +6,26 @@ projects.forEach(p => {
 
   card.innerHTML = `
     <img src="${p.image}" alt="${p.name}">
-    <h3>${p.name}</h3>
-    <p>${p.description}</p>
-    <a href="${p.github}" target="_blank">GitHub</a> |
-    <a href="${p.demo}" target="_blank">Live</a>
+
+    <div class="card-body">
+
+      <span class="card-tag">PROJECT</span>
+
+      <h3>${p.name}</h3>
+
+      <p>${p.description}</p>
+
+      <div style="margin-top:12px; display:flex; gap:10px;">
+        <a class="github-btn" href="${p.github}" target="_blank">
+          <i class="fa-brands fa-github"></i> Code
+        </a>
+
+        <a class="github-btn" href="${p.demo}" target="_blank">
+          Live
+        </a>
+      </div>
+
+    </div>
   `;
 
   container.appendChild(card);
--- js/projects.js
+++ js/projects.js
@@ -3,21 +3,28 @@ const projects = [
     name: "BookBliss",
     description: "Community platform for readers",
     image: "https://images.unsplash.com/photo-1512820790803-83ca734da794",
-    github: "#",
+    github: "https://github.com/nuhaaaaaaa24/BookBliss",
     demo: "#"
   },
   {
     name: "Membership System",
     description: "Spring Boot CRUD backend system",
     image: "https://images.unsplash.com/photo-1555949963-aa79dcee981c",
-    github: "#",
+    github: "https://github.com/nuhaaaaaaa24/membershipManagementSystem",
     demo: "#"
   },
   {
     name: "AquaTurtle",
     description: "Marine conservation awareness site",
     image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
-    github: "#",
+    github: "https://github.com/nuhaaaaaaa24/AquaTurtle",
+    demo: "#"
+  },
+  {
+    name:"Wanderlust",
+    description: "Travel blog and itinerary planner",
+    image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+    github: "https://github.com/nuhaaaaaaa24/Wanderlust_CC",
     demo: "#"
   }
 ];
